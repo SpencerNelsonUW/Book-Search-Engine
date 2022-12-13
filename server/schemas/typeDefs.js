7215543cb409f8560662d29326bc93d97573d6a2
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-12-13 15:15:59 -0800

    making and fixing typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,44 @@
-const gql = require('graphql-tag');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Skill{
-        id:ID!
-        title:String!
-        username:String!
-        currentHour:Int!
+
+type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String!
     }
-    type Query{
-        getSkills: [Skill]
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+     
+    input savedBook {
+        bookId: String!
+        authors: [String]
+        title: String!
+        description: String!
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
     }
 `;
 
